432262e67770485dbd9e109ad10c6ac432bc905c
Juan Miguel Acosta Ortega <acostaojuanmi@gmail.com>
2025-04-27 11:12:36 +0200

    fix : Estandarizando  la API según RFC 7231

--- src/main/java/com/calendarugr/mail_service/MailServiceApplication.java
+++ src/main/java/com/calendarugr/mail_service/MailServiceApplication.java
@@ -17,6 +17,10 @@ public class MailServiceApplication {
 		System.setProperty("MAIL_PASSWORD", dotenv.get("MAIL_PASSWORD"));
 		System.setProperty("API_KEY", dotenv.get("API_KEY"));
 		System.setProperty("EUREKA_URL", dotenv.get("EUREKA_URL"));
+		System.setProperty("RABBITMQ_HOST", dotenv.get("RABBITMQ_HOST"));
+		System.setProperty("RABBITMQ_PORT", dotenv.get("RABBITMQ_PORT"));
+		System.setProperty("RABBITMQ_USERNAME", dotenv.get("RABBITMQ_USERNAME"));
+		System.setProperty("RABBITMQ_PASSWORD", dotenv.get("RABBITMQ_PASSWORD"));
 		SpringApplication.run(MailServiceApplication.class, args);
 	}
 
