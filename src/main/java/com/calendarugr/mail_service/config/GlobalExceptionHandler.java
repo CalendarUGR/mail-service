ef3ed5540ca58acb93d29865d243f669ef30d042
Juan Miguel Acosta Ortega <acostaojuanmi@gmail.com>
2025-05-22 20:59:16 +0200

    fix : Introducing ErrorResponseDTO

--- src/main/java/com/calendarugr/mail_service/config/GlobalExceptionHandler.java
+++ src/main/java/com/calendarugr/mail_service/config/GlobalExceptionHandler.java
@@ -5,15 +5,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.calendarugr.mail_service.dtos.ErrorResponseDTO;
+
 import jakarta.mail.MessagingException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MessagingException.class)
-    public ResponseEntity<String> handleMessagingException(MessagingException e){
-        return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    public ResponseEntity<ErrorResponseDTO> handleMessagingException(MessagingException e){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponseDTO("MessagingException", e.getMessage()));
     }
-
 }
-
